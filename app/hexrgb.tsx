819cced8a6c1d87764302528494f8838fef46dec
mahmoudkhateep7-arch <mahmoudkhateep7@gmail.com>
2026-04-30 04:01:56 +0300

    made bg hex and rgb

---
+++ app/hexrgb.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { useEffect, useLayoutEffect, useState } from "react";
+const str = '123456789abcdef'
+const twocol = ['rgb(123,123,123)', '#414231']
+export default function HexRgb() {
+  const [aa, staa] = useState(1)
+  const [ss, sss] = useState(1)
+
+  const [num, setnum] = useState(Math.floor(Math.random() * 2))
+  const [col, setCol] = useState(num == 0 ? twocol[1] : twocol[0])
+  const randomLetterGenerator = () => {
+    const randomIndex = Math.floor(Math.random() * str.length)
+    const randomLetter = str[randomIndex]
+    return randomLetter
+  }
+  const createHexCol = () => {
+    const arrayofsixLetters = Array.from({ length: 6 }).map((n) => { return randomLetterGenerator() })
+    console.log((arrayofsixLetters.join(""))
+    )
+
+    return `#${arrayofsixLetters.join("")}`
+
+
+  }
+  const generateNumfrom0to255 = () => {
+    return Math.round(Math.random() * 255)
+  }
+  const rgbColGenrator = () => {
+    return `rgb(${generateNumfrom0to255()},${generateNumfrom0to255()},${generateNumfrom0to255()})`
+  }
+  const fn = () => {
+    if (num == 0) {
+      setCol(createHexCol())
+    } else {
+      setCol(rgbColGenrator())
+
+    }
+  }
+
+
+  useEffect(() => {
+    if (aa == 1) {
+
+    }
+    else {
+      fn()
+    }
+    const interval1 = setInterval(() => {
+      fn()
+    }, 4000);
+    return () => { clearInterval(interval1) }
+  }, [num, ss])
+  useEffect(() => {
+    const sett = setTimeout(() => {
+      staa(2)
+    }, 0);
+  }, [])
+  return (
+    <div style={{ background: col }} className={`py-6 `}>
+      {/* 3 buttons  */}
+      <div className={`flex justify-center  gap-5`}>
+        <div onClick={() => { setnum(0) }} className={`${num == 0 ? 'bg-black text-white border-white scale-115 ' : 'bg-white text-black border-black'} border cursor-pointer text-xl px-3 py-1.5`}>Create Hex color</div>
+        <div onClick={() => { setnum(1) }} className={`${num == 1 ? 'bg-black text-white border-white scale-115' : 'bg-white text-black border-black'} border cursor-pointer text-xl px-3 py-1.5`}>Create Rgb color</div>
+        <div onClick={() => { sss(Math.random()) }} className={`border border-black hover:bg-black hover:text-white hover:border-white cursor-pointer duration-300 transition-all flex items-center px-4 text-xl bg-white text-black`}>new color</div>
+      </div>
+
+      <div className={`text-4xl font-bold text-center  py-20`}>{num == 0 ? 'HEX Color' : 'RGB color'}</div>
+
+
+      <div className={`pt-10`}>
+        <p className={`text-white text-center text-7xl max-[1200px]:text-5xl max-[800px]:text-4xl max-[600px]:text-2xl`}>{col}</p>
+      </div>
+
+      <div></div>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import './style.css'
 import TopHeadr from "./topheader";
 import Title from "./title";
 import Accordian from "./accordian";
+import HexRgb from "./hexrgb";
 // an array here
 export default function Page() {
   const [index, setIndex] = useState(-2)
@@ -13,6 +14,7 @@ export default function Page() {
       <TopHeadr></TopHeadr>
       <Title></Title>
       <Accordian></Accordian>
+      <HexRgb></HexRgb>
 
     </div>
   )
